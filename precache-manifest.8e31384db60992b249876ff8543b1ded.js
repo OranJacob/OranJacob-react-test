d684712ec218d27bceb3c72f75c139178dc964f8
oran <oranjacob@levo.com.au>
2019-07-08 13:56:26 +0300

    Updates

---
+++ precache-manifest.8e31384db60992b249876ff8543b1ded.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "66cc936d2e888f60faba",
+    "url": "/OranJacob-react-test/static/css/main.91e0aa13.chunk.css"
+  },
+  {
+    "revision": "66cc936d2e888f60faba",
+    "url": "/OranJacob-react-test/static/js/main.66cc936d.chunk.js"
+  },
+  {
+    "revision": "9d1d8638bc7efc5f4161",
+    "url": "/OranJacob-react-test/static/css/1.a0d1b406.chunk.css"
+  },
+  {
+    "revision": "9d1d8638bc7efc5f4161",
+    "url": "/OranJacob-react-test/static/js/1.9d1d8638.chunk.js"
+  },
+  {
+    "revision": "689deb6fe6bb9f5fac04",
+    "url": "/OranJacob-react-test/static/js/runtime~main.689deb6f.js"
+  },
+  {
+    "revision": "4f459ca96aa73499ce6d8f240f3dee48",
+    "url": "/OranJacob-react-test/index.html"
+  }
+];
